7f5fe14d5fdaab5fe90b0276fbcca410f1f4bc30
tarTG <tgerdi@web.de>
2018-05-18 15:54:49 +0200

    fixed CI

--- src/simplerandom.cpp
+++ src/simplerandom.cpp
@@ -20,6 +20,9 @@
  *  License along with this library; if not, see http://www.gnu.org/licenses/
  *****************************************************************************/
 
+#define NOMINMAX
+
+
 #include "simplerandom.hpp"
 #include <stddef.h>
 #include "utils.hpp"
